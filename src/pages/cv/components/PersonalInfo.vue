4787da31d449a8936ceb5be52847fbad3ee68319
Jan Reiners <jan0411@web.de>
2025-01-21 22:52:15 +0100

    bump version to 1.0.0: added favicons, adjust icon sizes in PersonalInfo component, fixed spelling errors

--- src/pages/cv/components/PersonalInfo.vue
+++ src/pages/cv/components/PersonalInfo.vue
@@ -10,20 +10,20 @@ const emailAddress = import.meta.env.VITE_EMAIL_ADDRESS;
         <h2 class="text-gray-500 text-4xl ">Software Engineer</h2>
         <div>
             <div class="flex gap-1 items-center">
-                <img src="@/assets/github-icon.svg" alt="github" class="w-5 h-5">
+                <img src="@/assets/github-icon.svg" alt="github" class="w-4 h-4">
                 <a href="https://github.com/JanRnrs">https://github.com/JanRnrs</a>
             </div>
             <div class="flex gap-1 items-center">
-                <img src="@/assets/linkedin_black_logo_icon.svg" alt="linkedin" class="w-5 h-5 ">
+                <img src="@/assets/linkedin_black_logo_icon.svg" alt="linkedin" class="w-4 h-4 ">
                 <a href="https://www.linkedin.com/in/jan-reiners-98243620a/">https://www.linkedin.com/in/jan-reiners-98243620a/</a>
             </div>
             <div v-if="showContactDetails">
                 <div class="flex gap-1 items-center">
-                    <img src="@/assets/phone-icon.svg" alt="mobile number" class="w-5 h-5">
+                    <img src="@/assets/phone-icon.svg" alt="mobile number" class="w-4 h-4">
                     <p>{{ phoneNumber }}</p>
                 </div>
                 <div class="flex gap-1 items-center">
-                    <img src="@/assets/email-icon.svg" alt="email address" class="w-5 h-5 ">
+                    <img src="@/assets/email-icon.svg" alt="email address" class="w-4 h-4 ">
                     <p>{{ emailAddress }}</p>
                 </div>
             </div>
